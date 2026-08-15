77a2994cc3fab911d8fa7a2905501b7c5debc318
Kriston Rickman <kristonrickman@gmail.com>
2026-08-14 23:55:05 -0700

    Object detection with YOLO and cv2 testing on landmark one: flower vase

--- src/img_recogn_v1_01.py
+++ src/img_recogn_v1_01.py
@@ -1,31 +1,75 @@
 #Name: Kriston Rickman
 #Date: 08/13/26
-#V1.00
-#Notes: Testing if the camera works with the picamera2 library and if the camera is hooked properly
+#V1.01
+#Notes: /* Now testing the database with the raspberry pi 
+# through the library ultralytics YOLO. *\
 
+#/*Testing if the camera works with the picamera2 library
+#and if the camera is hooked properly.*\
 
+# /*sudo apt install -y python3-picamera2 to operate the
+# camera through the DSI connectors.*\
+
+# /*sudo apt install git -y to clone the program onto the
+# raspberry pi.*\
+
+# Libraries
 from picamera2 import Picamera2
+from ultralytics import YOLO
+import cv2
 import time
 
-camera = Picamera2()
-camera.start()
-time.sleep(2)
+
+#Global Variables
+print("initializing model...")
+model = YOLO("flwr_vase.pt")
+
+confidence = 0.4
+image_size = (640, 480)
+
+# /* The amount of frames to confirm 
+# the object is present in the image. *\
+frame_confirm = 5
 
 
-def capture_image(filename="snapshot_test.jpg"):
+def main():
+    #local variables
+    count = 0
+
+    camera = Picamera2()
+    camera.configure(
+        camera.create_preview_configuration(
+            main ={"format": "RGB888", "size": (640, 480)}
+            )
+        )
+    camera.start()
+    time.sleep(2)
+
     try:
-        camera.capture_file(filename)
-        print(f"Image saved as {filename}")
-    except Exception as e:
-        print(f"Failed to save image: {e}")
+        while True:
 
+            frame = camera.capture_array()
 
-if __name__ == "__main__":
-    try:
-        capture_image("snapshot_test.jpg")
-    except KeyboardInterrupt:
-        print("Exiting...")
+            results = model(
+                frame,
+                conf=confidence,
+                imgsz=image_size
+                )
+            frame_annotated = results[0].plot()
+            cv2.imshow("Showing input...", frame_annotated)
+            if len(results[0].boxes) > 0:
+                count += 1
+
+                if count >= frame_confirm:
+                    print("Object detected!")
+                    count = 0
+            else:
+                # /*Detection was lost from the camera, thus 
+                # resetting the counter*\
+                count = 0
+            if cv2.waitKey(1) == ord("q"):
+                break
     finally:
+        print("Disconnecting from program...")
+        cv2.destroyAllWindows()
         camera.stop()
-
-
