639a85d5cfbdf38365bbd472aeb8dab8811be15d
Kriston Rickman <kristonrickman@gmail.com>
2026-08-21 00:14:28 -0700

    Add files via upload

---
+++ src/navigation_v1_01.ino
@@ -0,0 +1,116 @@
+//Name: Kriston Rickman
+//Date: 08/20/26
+//V1.01
+//Note: /*I made a mistake with creating my arduino
+// code on hte python platform and transferring over*\
+
+//Libraries
+#include <Servo.h>
+
+//variables
+const byte navtrigpin = 1;
+const byte 
+
+//States
+enum class State {
+    off,
+    search,
+    center,
+    LM1,
+    LM2,
+    LM3,
+    NUM_STATES
+};
+
+//Events
+enum class Event {
+    btn_pushed,
+    obstacle,
+    LM1_found,
+    LM2_found,
+    LM3_found,
+    NUM_EVENTS
+};
+
+//The total number of states and events
+const int NUM_STATES = static_cast<int>(State::NUM_STATES);
+const int NUM_EVENTS = static_cast<int>(Event::NUM_EVENTS);
+
+//Transition table
+State transitionTable[NUM_STATES][NUM_EVENTS] = {
+    //btn_push,       Obstacles,         LM1_found        LM2_found,       LM3_found
+    {State::center,  State::center,      State::off,      State::off,      State::off },  // off
+    {State::off,     State::center,      State::LM1,      State::LM2,      State::LM3 },  // search
+    {State::off,     State::center,      State::LM1,      State::LM2,      State::LM3 },  // center
+    {State::off,     State::center,      State::LM1,      State::LM2,      State::LM3 },  // LM1
+    {State::off,     State::center,      State::LM1,      State::LM2,      State::LM3 },  // LM2
+    {State::off,     State::center,      State::LM1,      State::LM2,      State::LM3 }   // LM3
+};
+
+
+void setup() {
+  Serial.begin(11520);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
+
+void centerOnLandmark()
+{
+    int cameraAngle = targetServoAngle;
+
+    // Landmark is to the LEFT of robot
+    if (cameraAngle < 90)
+    {
+        turnLeft();
+
+        // Keep camera pointed at landmark
+        cameraAngle += 2;
+        cameraServo.write(cameraAngle);
+    }
+
+    // Landmark is to the RIGHT of robot
+    else if (cameraAngle > 90)
+    {
+        turnRight();
+
+        // Keep camera pointed at landmark
+        cameraAngle -= 2;
+        cameraServo.write(cameraAngle);
+    }
+
+    // Landmark is directly ahead
+    else
+    {
+        stopMotors();
+    }
+}
+
+
+//This code is based off Tin's functions and will work when integrating all code together
+// Turn robot left
+void turnLeft()
+{
+  rightmotor(150);
+  leftmotor(-150);
+}
+
+// Turn robot right
+void turnRight()
+{
+  rightmotor(-150);
+  leftmotor(150);
+}
+
+// Stop both motors
+void stopMotor()
+{
+  rightmotor(0);
+  leftmotor(0);
+}
+
